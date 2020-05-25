2ed0d3627f98317fc8fc51f2dd7d9394abf8ecba
Pritam Pramanik <pritam@involvio.com>
2020-05-25 23:59:01 +0530

    Updates

---
+++ precache-manifest.8770accb58040ee6eddcaf16f13bd29e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6df9a6207845275b0c5e2c4463eb893",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae6e24fad6d93b6ea87b",
+    "url": "/portfolio/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "737471b13b9841c3539d",
+    "url": "/portfolio/static/js/2.b53665f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b53665f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6e24fad6d93b6ea87b",
+    "url": "/portfolio/static/js/main.04849fca.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/portfolio/static/media/logo.8dad8028.svg"
+  }
+]);
